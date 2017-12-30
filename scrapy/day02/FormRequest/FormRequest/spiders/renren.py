d64fc0be0439ea054607478b63e33d1da866e5d3
jing <girlgps@gmail.com>
2017-12-30 11:52:11 +0800

    scrapy发送post请求并记录cookie，模拟登录人人网

--- scrapy/day02/FormRequest/FormRequest/spiders/renren.py
+++ scrapy/day02/FormRequest/FormRequest/spiders/renren.py
@@ -5,7 +5,28 @@ import scrapy
 class RenrenSpider(scrapy.Spider):
     name = 'renren'
     allowed_domains = ['renren.com']
-    start_urls = ['http://renren.com/']
+    # start_urls = ['http://renren.com/PLogin.do']
+
+    def start_requests(self):
+        # 如果重写了start_requests，那么start_urls可有可无
+        login_url = 'http://renren.com/PLogin.do'
+        yield scrapy.FormRequest(
+            url=login_url,
+            formdata={
+                'email': 'xxx',
+                'password': 'xxx'
+            },
+            callback=self.parse
+        )
 
     def parse(self, response):
-        pass
+        url_list = ['http://www.renren.com/410043129/profile',
+                    'http://www.renren.com/429732223/profile']
+
+        for url in url_list:
+            yield scrapy.Request(url=url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        file_name = response.xpath('//title/text()').extract_first() + '.html'
+        with open(file_name, 'w') as f:
+            f.write(response.body)
